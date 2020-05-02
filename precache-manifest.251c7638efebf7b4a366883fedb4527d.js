f69f5d3a6303be49f6cd8173c17e71808aee937a
Pranjal Varshney <Pranjal Varshney>
2020-05-03 01:00:02 +0530

    Updates

---
+++ precache-manifest.251c7638efebf7b4a366883fedb4527d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4cb78b26c1aa675050d9775e18b7aa32",
+    "url": "/todo-react-app/index.html"
+  },
+  {
+    "revision": "0f9aab476a6653354e85",
+    "url": "/todo-react-app/static/css/main.4de1d46f.chunk.css"
+  },
+  {
+    "revision": "8776fe9b91a3808b69f4",
+    "url": "/todo-react-app/static/js/2.237a9f09.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo-react-app/static/js/2.237a9f09.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f9aab476a6653354e85",
+    "url": "/todo-react-app/static/js/main.9681a4d1.chunk.js"
+  },
+  {
+    "revision": "a0fbcc1a8129889c15eb",
+    "url": "/todo-react-app/static/js/runtime-main.2e1b0e70.js"
+  }
+]);
